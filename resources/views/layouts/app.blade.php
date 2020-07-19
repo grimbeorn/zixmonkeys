e0837788709ae2964b2901027b909225ab85ea40
root <root@DESKTOP-Q780FOQ.localdomain>
2020-07-19 00:19:18 -0500

    cliengo 04

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -85,7 +85,7 @@
       
       <!-- FOOTER -->
       <footer>
-        <!-- Código de instalación Cliengo para hernandomtz@gmail.com --> <script type="text/javascript">(function () { var ldk = document.createElement('script'); ldk.type = 'text/javascript'; ldk.async = true; ldk.src = 'https://s.cliengo.com/weboptimizer/5f13cc0c4c8b34002a11e4c0/5f13cc0e4c8b34002a11e4c3.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ldk, s); })();</script>
+
         <!-- Contact Us -->
         <!-- <div class="background-dark padding text-center footer-social">
           <a class="margin-right-10" target="_blank" href="https://www.facebook.com"><i class="icon-facebook_circle text-size-30"></i> <span class="text-strong text-white hide-s hide-m">FACEBOOK</span></a>
@@ -175,5 +175,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
+    <!-- Código de instalación Cliengo para hernandomtz@gmail.com --> <script type="text/javascript">(function () { var ldk = document.createElement('script'); ldk.type = 'text/javascript'; ldk.async = true; ldk.src = 'https://s.cliengo.com/weboptimizer/5f13cc0c4c8b34002a11e4c0/5f13cc0e4c8b34002a11e4c3.js'; var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ldk, s); })();</script>
+    
   </body>
 </html>
