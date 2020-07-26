55dbb05860593b6a03c5646c635aee82dfaa90f0
grimbeorn <hernandomtz@gmail.com>
2020-07-26 10:50:32 -0500

    faqs 05

--- resources/views/faqs.blade.php
+++ resources/views/faqs.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>01</b> ¿Se puede optimizar mi estructura?</h2>
-          <p>entendemos y empatamos el proyecto aquitectónico con las necesidades de la ingeniería estructural. De hecho, somos partidarios de que ambas disciplinas se complementan, no se repelen.</p>                
+          <p>Claro que es posible! Si se hacen las consideraciones estructurales adecuadas, es posible optimizar casi cualquier estructura.</p>                
         </div> 
       </div>
       <div class="s-12 m-12 l-12 margin-bottom-30">
@@ -29,7 +29,7 @@
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>02</b> ¿Se pueden reducir costos en la estructura que tengo calculada actualmente?</h2>
-          <p>definimos objetivos, planteamos variables y restricciones y nos abocamos a optimizar las estructuras, no sólo desde el punto de vista material, sino constructivo o de plazos.</p>                
+          <p>Al optimizarl la estructura, automáticamente se optimizan los costos, por lo que sin duda se pueden reducir los costos.</p>                
         </div> 
       </div>
       <div class="s-12 m-12 l-12 margin-bottom-30">
@@ -38,7 +38,7 @@
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>03</b> ¿Qué tipo de estructura me conviene, de acero o de concreto?</h2>
-          <p>pensamos en estructuras que se puedan construir facilmente, y esto lo expresamos en planos claros y precisos.</p>                
+          <p>Esto lo tendrá que decidir tu equipo pues depende del tipo de proyecto, presupuesto, tiempo de ejecución y condiciones arquitectónicas. Lo que sí te podemos decir, es que ambos sistemas son muy buenos y funcionales.</p>                
         </div> 
       </div>
       <div class="s-12 m-12 l-12 margin-bottom-30">
@@ -47,52 +47,52 @@
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>04</b> ¿Cuál es el mejor sistema para construir rápidamente mi obra?</h2>
-          <p>asumimos e incorporamos en la estructura las implicaciones, afecciones y necesidades de las demás ingenierías (hidráulica, eléctrica, ascensores...) </p>                
+          <p>Dependiendo del tipo de edificación (dimensiones, altura, uso, etc.) es probable que debas ejecutarla en acero, sin embargo, hay varias alternativas y nosotros te podemos asesorar sobre la mejor decisión. </p>                
         </div> 
       </div>
-      <div class="s-12 m-12 l-12 margin-m-bottom-30">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
         <div class="float-left hide-m hide-s">
-          <i class="icon-sli-home text-size-40 text-dark"></i>
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>05</b> ¿Es necesario pagar tanto por el cálculo estructural?</h2>
-          <p>por supuesto, las normas vigentes de aplicaciones están siempre presentes en nuestro quehacer, no sólo para obedecerlas sino para saber en qué casos pueden favorecer a los proyectos.</p>                
+          <p>Aunque es una parte escencial en tu proyecto, no es necesario pagar costos excesivos. Ten en cuenta que al optimizar los costos de la estructura, el proyecto estructural se absorben en dichos ahorros. </p>                
         </div> 
       </div>
-      <div class="s-12 m-12 l-12 margin-m-bottom-30">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
         <div class="float-left hide-m hide-s">
-          <i class="icon-sli-grid text-size-40 text-dark"></i>
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>06</b> ¿Cuánto me puedo ahorrar con el cálculo de la estructura?</h2>
-          <p>ayudamos a la toma de decisiones por medio de la valoración de conceptos y proyectos, resolviendo dudas del tipo: ¿qué estructura puede ser mejor acero o concreto?, ¿qué tipo de losa puede convenir unidireccional o bidireccional?</p>                
+          <p>Aunque no podemos responder esto de forma exacta, lo que sí podemos decirte es que según nuestra experiencia, mínimamente podemos lograr ahorros de un 5% a 8%. </p>                
         </div> 
       </div>
-      <div class="s-12 m-12 l-12 margin-m-bottom-30">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
         <div class="float-left hide-m hide-s">
-          <i class="icon-sli-crop text-size-40 text-dark"></i>
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>07</b> ¿Se pueden realizar todas las ideas arquitectónicas?</h2>
-          <p>dentro de nuestro quehacer está: from the scratch, recálculos, informes, reformas, remodelaciones, refuerzos, asistencia técnica, industrialización y fichas técnicas, asesorías. </p>                
+          <p>Bueno, talvez no todas! pero hasta el día de hoy, hemos podido satisfacer las demandas más exigentes de nuestros clientes!</p>                
         </div> 
       </div>
-      <div class="s-12 m-12 l-12 margin-m-bottom-30">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
         <div class="float-left hide-m hide-s">
-          <i class="icon-sli-vector text-size-40 text-dark"></i>
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>08</b> ¿Porqué me conviene más un estructurista u otro?</h2>
-          <p>comunicar, explicar, entender, compartir lo que hacemos, porqué lo hacemos y cómo lo hacemos con el cliente, es parte esencial de nuestra visión y forma de trabajo.</p>                
+          <p>Lo que más te conviene es trabajar con un estructurista que entienda el proyecto como un todo, no como un calculista. Esto implica que sea capaz de visualizar los proyectos con la óptica del arquitecto y su equipo. Dicho de otra forma, te conviene trabajar con el estructurista que mejor entienda tus necesidades. </p>                
         </div> 
       </div>
-      <div class="s-12 m-12 l-12 margin-m-bottom-30">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
         <div class="float-left hide-m hide-s">
-          <i class="icon-sli-vector text-size-40 text-dark"></i>
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
         </div>
         <div class="margin-left-70 margin-m-left-0">
           <h2 class="text-size-20 margin-bottom-10"><b>09</b> ¿Se pueden cumplir todas las normativas del diseño estructural y además ser una estructura económica?</h2>
-          <p>comunicar, explicar, entender, compartir lo que hacemos, porqué lo hacemos y cómo lo hacemos con el cliente, es parte esencial de nuestra visión y forma de trabajo.</p>                
+          <p>Sin duda es posible, de hecho, aunque los reglamentos siempre velan por la seguridad estructural, también permiten ir más allá para optimizar los diseños. Para ello, es necesario que el estructurista tenga un profundo conocimiento de los reglamentos y de las consideraciones detrás de ellos. Si esto es posible, pueden lograrse estructuras livianas y seguras. </p>                
         </div> 
       </div>
     </div>
