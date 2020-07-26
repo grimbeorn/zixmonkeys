51f0326f49af3a609a62034df9315a1c675ac48f
grimbeorn <hernandomtz@gmail.com>
2020-07-26 10:22:55 -0500

    faqs 01

---
+++ resources/views/faqs.blade.php
@@ -0,0 +1,109 @@
+@extends('layouts.app')
+
+@section('content')
+
+<article>
+  <header class="section" data-image-src="img/bsp/38.jpg">
+    <div class="line text-center">
+      <h1 class="margin-bottom-40 text-thin text-white text-s-size-30 text-m-size-40 text-size-headline text-line-height-1"><span class="text-white"><b>-</b></span><b> FAQ </b><span class="text-white"><b>-</b></span></h1>
+      <!-- <p class="margin-bottom-0 text-size-20 text-white">
+        El valor de nuestro trabajo es la empatía con las necesidades arquitectónicas, estéticas y económicas de nuestros clientes.
+      </p> -->
+    </div>  
+  </header>
+  
+  <section class="section full-width background-white">
+    <div class="margin2x">
+      <div class="s-12 m-12 l-12 margin-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-layers text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>01</b> ¿Se puede optimizar mi estructura?</h2>
+          <p>entendemos y empatamos el proyecto aquitectónico con las necesidades de la ingeniería estructural. De hecho, somos partidarios de que ambas disciplinas se complementan, no se repelen.</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-12 margin-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-pin text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>02</b> ¿Se pueden reducir costos en la estructura que tengo calculada actualmente?</h2>
+          <p>definimos objetivos, planteamos variables y restricciones y nos abocamos a optimizar las estructuras, no sólo desde el punto de vista material, sino constructivo o de plazos.</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-12 margin-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-pencil text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>03</b> ¿Qué tipo de estructura me conviene, de acero o de concreto?</h2>
+          <p>pensamos en estructuras que se puedan construir facilmente, y esto lo expresamos en planos claros y precisos.</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-3 margin-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-paper-plane text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>04</b> ¿Cuál es el mejor sistema para construir rápidamente mi obra?</h2>
+          <p>asumimos e incorporamos en la estructura las implicaciones, afecciones y necesidades de las demás ingenierías (hidráulica, eléctrica, ascensores...) </p>                
+        </div> 
+      </div>
+    </div>
+    <div class="margin2x">
+      <div class="s-12 m-12 l-3 margin-m-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-home text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>05</b> ¿Es necesario pagar tanto por el cálculo estructural?</h2>
+          <p>por supuesto, las normas vigentes de aplicaciones están siempre presentes en nuestro quehacer, no sólo para obedecerlas sino para saber en qué casos pueden favorecer a los proyectos.</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-3 margin-m-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-grid text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>06</b> ¿Cuánto me puedo ahorrar con el cálculo de la estructura?</h2>
+          <p>ayudamos a la toma de decisiones por medio de la valoración de conceptos y proyectos, resolviendo dudas del tipo: ¿qué estructura puede ser mejor acero o concreto?, ¿qué tipo de losa puede convenir unidireccional o bidireccional?</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-3 margin-m-bottom-30">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-crop text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>07</b> ¿Se pueden realizar todas las ideas arquitectónicas?</h2>
+          <p>dentro de nuestro quehacer está: from the scratch, recálculos, informes, reformas, remodelaciones, refuerzos, asistencia técnica, industrialización y fichas técnicas, asesorías. </p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-3">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-vector text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>08</b> ¿Porqué me conviene más un estructurista u otro?</h2>
+          <p>comunicar, explicar, entender, compartir lo que hacemos, porqué lo hacemos y cómo lo hacemos con el cliente, es parte esencial de nuestra visión y forma de trabajo.</p>                
+        </div> 
+      </div>
+      <div class="s-12 m-12 l-3">
+        <div class="float-left hide-m hide-s">
+          <i class="icon-sli-vector text-size-40 text-dark"></i>
+        </div>
+        <div class="margin-left-70 margin-m-left-0">
+          <h2 class="text-size-20 margin-bottom-10"><b>08</b> ¿Se pueden cumplir todas las normativas del diseño estructural y además ser una estructura económica?</h2>
+          <p>comunicar, explicar, entender, compartir lo que hacemos, porqué lo hacemos y cómo lo hacemos con el cliente, es parte esencial de nuestra visión y forma de trabajo.</p>                
+        </div> 
+      </div>
+    </div>
+  </section>
+  
+  <hr class="margin-top-bottom-0">
+  
+
+</article>
+
+        
+@endsection
